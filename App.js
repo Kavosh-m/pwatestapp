de42ae24b8f694990dd1a063387b0009d9753e5c
Kavosh Madadi <kavoshmadadiglk@gmail.com>
2022-12-08 10:43:42 +0330

    - commit add text

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 
+import * as serviceWorkerRegistration from "./src/serviceWorkerRegistration";
+
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
+        <Text style={styles.text}>Hi there!</Text>
         <Image source={PlaceholderImage} style={styles.image} />
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 const styles = StyleSheet.create({
   container: {
@@ -23,6 +31,13 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    fontStyle: "italic",
+    color: "#fff",
+    marginVertical: 20,
   },
   image: {
     width: 320,
